c00c72a16305535206a40759e6f4c7e317f857f5
EgiKelo9 <159183576+EgiKelo9@users.noreply.github.com>
2024-07-02 09:01:37 +0800

    Add files via upload

---
+++ PatientQueue.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+
+public class PatientQueue {
+
+    private QueueNode front;
+    private QueueNode rear;
+    private ArrayList<Patient> originalOrder;
+
+    public PatientQueue() {
+        this.front = null;
+        this.rear = null;
+    }
+
+    public void enqueue(Patient patient) {
+        QueueNode newNode = new QueueNode(patient);
+        if (rear == null) {
+            front = newNode;
+            rear = newNode;
+            return;
+        }
+        rear.next = newNode;
+        rear = newNode;
+    }
+
+    public void dequeue() {
+        if(front == null) return;
+        front = front.next;
+        if(front == null) rear = null;
+    }
+
+    public boolean isEmpty() {
+        return front == null;
+    }
+
+    public QueueNode getFront() {
+        return front;
+    }
+
+    public ArrayList<Patient> getOriginalOrder() {
+        originalOrder = getCurrentOrder();
+        return originalOrder;
+    }
+
+    public ArrayList<Patient> getCurrentOrder() {
+        ArrayList<Patient> currentList = new ArrayList<>();
+        QueueNode currentNode = front;
+        while (currentNode != null) {
+            currentList.add(currentNode.patient);
+            currentNode = currentNode.next;
+        }
+        return currentList;
+    }
+
+}
